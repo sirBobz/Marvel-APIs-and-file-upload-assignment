f4c2484c4f9a37cb2de62ba68df1ebb9be7f5ce3
Bob Mwenda <bmwenda@mgic.umaryland.edu>
2021-09-10 09:37:08 +0300

    added the sales table

---
+++ database/migrations/2021_09_10_062639_create_sales_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('InvoiceNo');
+            $table->string('StockCode');
+            $table->text('Description');
+            $table->string('Quantity');
+            $table->string('InvoiceDate');
+            $table->string('UnitPrice');
+            $table->bigInteger('CustomerID');
+            $table->string('Country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
