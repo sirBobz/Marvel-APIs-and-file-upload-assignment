c25dc544beeccb81f18e550eb54e7b4c371aee15
Bob Mwenda <bmwenda@mgic.umaryland.edu>
2021-09-11 03:05:47 +0300

    refined the user interface

--- resources/views/V1/API/index.blade.php
+++ resources/views/V1/API/index.blade.php
@@ -17,9 +17,9 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
             @foreach ($characters as $character)
                 <div class="character mt-8">
-                    <a href="{{ url("/apis/".$character['id'] ) }}"><img src="{{ $character['thumbnail'] }}" alt="{{ $character['name'].' avatar' }}" class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                    <img src="{{ $character['thumbnail'] }}" alt="{{ $character['name'].' avatar' }}" class="hover:opacity-75 transition ease-in-out duration-150">
                     <div class="mt-2">
-                        <a href="{{ url("/apis/".$character['id'] ) }}" class="text-lg mt-2 hover:text-gray-300">{{ $character['name'] }}</a>
+                        <div class="text-sm text-black-600 font-bold">{{ $character['name'] }}</div>
                         <div class="text-sm truncate text-gray-400">{{ $character['comics'] }}</div>
                     </div>
                 </div>
